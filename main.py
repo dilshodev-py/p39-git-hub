c36602603aee9edd4ea1754b26d29e9125fcfb7d
dilshodev-py <absaitovdev@gmail.com>
2026-04-20 17:38:40 +0500

    nimadir uzgardi

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 print("hello world")
 
 print(2 + 2)
+
+
+
+"""
+
+Send Email Message
+    main.py <- email habar yuboradigan code
+    .env    <- password , email  
+    .gitignore <- .idea , .venv , .env
+    
+    
+    push -> git hub
+
+
+"""
